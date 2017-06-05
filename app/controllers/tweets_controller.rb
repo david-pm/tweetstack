fdb52f545fe31e9f4c120859eb47f628d0e05b9d
davidpm <davidpm.io.web@gmail.com>
2017-06-04 22:15:01 -0700

    rescue 404 and 401, adjust markup

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,15 @@
 class TweetsController < ApplicationController
   def index
-    if params[:handle]
-      @tweets = $client.user_timeline(params[:handle], count: 25)
-    end
-
+    @tweets = $client.user_timeline(sanitize_handle, count: 25) if params[:handle]
+  rescue Twitter::Error::NotFound => e
+    flash.now[:danger] = "#{params[:handle]} is not a valid Twitter handle"
+  rescue Twitter::Error::Unauthorized => e
+    flash.now[:danger] = "Sorry, searching for #{params[:handle]} is unauthorized"
+  ensure
     render :index
+  end
+
+  private def sanitize_handle
+    params[:handle].gsub("@", "")
   end
 end
